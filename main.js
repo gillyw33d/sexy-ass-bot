d74a9800f49f8c3f7dc058e09054d23bd3b9e71c
gillyw33d <73448684+gillyw33d@users.noreply.github.com>
2020-11-02 16:57:42 -0600

    Add files via upload
    added logging

--- main.js
+++ main.js
@@ -9,6 +9,9 @@ const uptime = Date.now();
 const gillyID = '401534600741912576';
 
 const radlibs = new Array('vaush', 'xander', 'contra', 'contrapoints', 'philosophytube', 'olly', 'thoughtslime', 'thought slime', 'philosophy tube', 'xanderhal');
+const curse = new Array('fuck', 'shit', 'pussy', 'cunt', 'nigga', 'nigger', 'dick', 'ass', 'bitch', 'cock', 'damn', 'piss', 'tit', 'bastard', 'wanker', 'twat');
+
+
 
 const winston = require('winston');
 const logger = winston.createLogger({
@@ -19,6 +22,10 @@ const logger = winston.createLogger({
 });
 
 
+var today = new Date();
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+var today = new Date();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 
 
 
@@ -216,11 +223,13 @@ client.on('message', message =>{
 		// console.log(message.mentions.users);
         console.groupEnd();
 		
-		var output = message.author.username + ' ' + message.author.id + ' \n ' + message.content;
-		logger.log({
-			level: 'info',
-			message: output,
-		});
+		if (!(message.author.bot)) {
+			var output = message.author.username + ' ' + message.channel.name + ' ' + message.content;
+			logger.log({
+				level: 'info',
+				message: output,
+			});
+		}
 	// channel.send('user:' + message.author.username + ' ' + message.author.id);
 	// channel.send('channel:' + message.channel.name);
 	// channel.send('guild:' + message.guild);
@@ -239,6 +248,72 @@ client.on('message', message =>{
 //            .catch(console.error);
 //    }
 
+
+	if ((messageCheck.includes('||') || messageCheck.includes('`') || messageCheck.includes('**') || messageCheck.includes('_') || messageCheck.includes('~')) && (!(message.author.bot))) {
+		
+		if (!(message.guild.id == '748381226175561729')) break;
+		
+		/*switch(messageCheck) {
+			case includes('||'):
+				var content = messageCheck.split('|').join('');
+				console.log('i have reached this step');	
+				break;
+			case '`':
+				var content = messageCheck.split('`').join('');
+				break;
+			case '**':
+				var content = messageCheck.split('*').join('');
+				break;
+			default:
+				break;
+		}*/
+		if(messageCheck.includes('||')) {
+			var content = messageCheck.split('|').join('');
+		}else if(messageCheck.includes('`')) {
+			var content = messageCheck.split('`').join('');
+		}else if(messageCheck.includes('**')) {
+			var content = messageCheck.split('*').join('');
+		}else if(messageCheck.includes('_')) {
+			var content = messageCheck.split('_').join('');
+		}else if(messageCheck.includes('~')) {
+			var content = messageCheck.split('~').join('');
+		}
+
+			
+		
+		const nsfwLogEmbed = new Discord.MessageEmbed()
+			.setTitle('NSFW Log')
+			.setColor('#4EC577')
+			.setDescription('user: ' + message.author.username + ' \n id: ' + message.author.id + '\n channel: ' + '<#' + message.channel.id + '>' + /*' id: ' + message.channel.id + */ '\n message: ' + content)
+			.setFooter('Message logged by swag lord gilly#0492')
+			.setTimestamp();
+		
+		
+		
+		// client.channels.cache.get('772835458416902146').send('user: ' + message.author.username + ' ' + message.author.id + '\n channel: ' + message.channel.name + ' ' + message.channel.id + '\n message: '+ message.content + '\n Time: ' + date + ' ' + time);
+		client.channels.cache.get('772835458416902146').send(nsfwLogEmbed);
+	}
+
+
+
+
+	for (let i = 0; i < curse.length; i ++) {
+		if((message.author.id == gillyID) || message.author.bot || !(message.guild.id == '748381226175561729')) break;
+		if(messageCheck/*.split(' ').join('')*/.includes(curse[i])) {
+			// console.log(messageCheck.includes(curse[i]));
+			const curseEmbed = new Discord.MessageEmbed()
+				.setTitle('Curse Word Log')
+				.setColor('#0713FF')
+				.setDescription('user: ' + message.author.username + ' \n id: ' + message.author.id + '\n channel: ' + '<#' + message.channel.id + '>' +  '\n message: ' + message.content)
+				.setFooter('Message logged by swag lord gilly#0492')
+				.setTimestamp();
+			client.channels.cache.get('772858701848510464').send(curseEmbed);
+		}
+	}
+
+
+
+
     if (messageCheck == 'lol ') message.channel.send('league of legends :3');
 	if (messageCheck.includes('penis')) message.channel.send('8==================D');
 	if (messageCheck.includes('boob')) message.channel.send('o o');
@@ -246,9 +321,9 @@ client.on('message', message =>{
     if (messageCheck == 'wow') message.channel.send('world of warcraft >:3');
     // if (messageCheck.includes('stfu')) message.channel.send('stuf whore ' + userMention);
 	if (messageCheck == 'sup' && !message.author.bot) message.channel.send('hey lol :smirk: :flushed: ad me lol :flushed: :rofl:');
-	if (messageCheck.includes('nigger') || messageCheck.includes('nigga')) {
-		message.channel.send(userMention + ' is a racist fuck :clown:');
-	}
+	// if (messageCheck.includes('nigger') || messageCheck.includes('nigga')) {
+		// message.channel.send(userMention + ' is a racist fuck :clown:');
+	// }
 	if (messageCheck.split(' ').join('').includes('nigger') || messageCheck.split(' ').join('').includes('nigga')) {
 		// message.delete();
 		message.channel.send(userMention + ' is a racist fuck :clown:')
@@ -256,7 +331,7 @@ client.on('message', message =>{
 	}
 	
 	for (var i = 0; i < radlibs.length; i ++){
-		if (message.author.id == '401534600741912576') break;
+		if (message.author.id == gillyID) break;
 		if (messageCheck.includes(radlibs[i])) {
 			return message.channel.send("fuck yui radlib watcher absolute scum of leftism you are a stani on society fuck you i hate you \>:(");
 		}
@@ -266,9 +341,8 @@ client.on('message', message =>{
 	
 	
 	
-	if (messageCheck.includes('femcel') && !message.author.bot) message.channel.send('femcel is so fucking hot i want her so fucking bad if i could have one day with her oh what i would do. the sex would be immaculate, incredible, revolutionary even. then, the cuddles. oh how we would cuddle. hours of us holding each other, never letting go. i\'m quite turned on just thinking about it to be honest. we would fall asleep in each other\'s arms, me, the dom, obviously being the big spoon, would hold her tight to my chest. smelling her hair, maybe a butt clench or two.');
-	if (messageCheck.includes('dale') && !message.author.bot) message.channel.send('oh my god dale is so fucking hot i want him so fucking bad. when he goes schizo and starts speaking russian it makes me so fucking hrony. i mean uhhh... (づ｡◕‿‿◕｡)づ cummies pls (づ｡◕‿‿◕｡)づ');
+	// if (messageCheck.includes('femcel') && !message.author.bot) message.channel.send('femcel is so fucking hot i want her so fucking bad if i could have one day with her oh what i would do. the sex would be immaculate, incredible, revolutionary even. then, the cuddles. oh how we would cuddle. hours of us holding each other, never letting go. i\'m quite turned on just thinking about it to be honest. we would fall asleep in each other\'s arms, me, the dom, obviously being the big spoon, would hold her tight to my chest. smelling her hair, maybe a butt clench or two.');
+	// if (messageCheck.includes('dale') && !message.author.bot) message.channel.send('oh my god dale is so fucking hot i want him so fucking bad. when he goes schizo and starts speaking russian it makes me so fucking hrony. i mean uhhh... (づ｡◕‿‿◕｡)づ cummies pls (づ｡◕‿‿◕｡)づ');
 	
 })
 
-client.login('');
